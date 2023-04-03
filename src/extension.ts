c04fe3c22f3a8acb964c3c2f8f9eab0b3254c52d
Arpan Datta <arpan@codebuddy.co>
2023-04-03 12:07:17 +0530

    feat: injector wrapper added

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
-import { commands, ExtensionContext, Position, window, workspace, WorkspaceConfiguration } from "vscode";
+import { commands, ExtensionContext, window, workspace, WorkspaceConfiguration } from "vscode";
 import { Decorator } from "./decorator";
 import { DecoratorValue } from "./decoratorValue";
 import { Commands, Configs } from "./enums";
 import { EventsLimit } from "./utils";
 import axios from "axios";
 import { InjectorIdType } from "./types";
+import { runInjectorIdWrapCommand } from "./injectorIdWrap";
 
 const loadDataFromApi = async () => {
    const res = await axios.get<{result: Array<InjectorIdType>}>('http://localhost:3000/test/injector-id');
@@ -40,44 +41,9 @@ export async function activate(context: ExtensionContext) {
       decoratorValue.toggle();
    }, null);
    
+   // Command register for injector id wrap.
    commands.registerCommand(Commands.injectorIdInsert, async () => {
-      const selectedId = await window.showQuickPick(injectorIds);
-
-      if (selectedId === undefined) return;
-
-      const injectorIdPart = selectedId.injectorId.split('-');
-
-      let searchQuery = '';
-      if (injectorIdPart.length === 2) {
-         searchQuery = await window.showInputBox({
-            placeHolder: "Enter name",
-            prompt: `Enter ${injectorIdPart[1].replace('{', '').replace('}', '')}`
-         });
-
-         if (searchQuery === undefined) return;
-      }
-
-      const activeTextEditor = window.activeTextEditor;
-      const injectorId = injectorIdPart.length === 2 ? `${injectorIdPart[0]}-${searchQuery}` : selectedId?.injectorId;
-
-      const selectePosition = activeTextEditor.selections;
-      const prefixText = `/** @injector-start: ${injectorId} */`;
-      const safixText = `/** @injector-stop: ${injectorId} */`;
-
-      activeTextEditor.edit(editBuilder => {
-         const selectedText0 = activeTextEditor.document.lineAt(selectePosition[0].start.line).text;
-         const selectedText1 = activeTextEditor.document.lineAt(selectePosition[0].end.line).text;
-
-         editBuilder.insert(
-            new Position(selectePosition[0].start.line, selectedText0.search(/\S/)),
-            `${prefixText}\n${selectedText0.substring(0, selectedText0.search(/\S/))}`
-         );
-         editBuilder.insert(
-            new Position(selectePosition[0].end.line, selectedText1.length + selectedText1.search(/\S/)),
-            `\n${selectedText1.substring(0, selectedText1.search(/\S/))}${safixText}`
-         );
-      });
-
+      runInjectorIdWrapCommand(injectorIds);
    }, null);
 
    window.onDidChangeActiveTextEditor((e) => {
